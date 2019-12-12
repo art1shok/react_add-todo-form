3fc3b41051ba1ffc7cd1331ddbebd9ef5114c6a1
art1shok <kvitkaart554@gmail.com>
2019-12-12 10:03:42 +0200

    Updates

---
+++ precache-manifest.e39ce6b20709055cd99259e33d59f6fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce21f641a6d5059fe37adc99af808f26",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "a214322a08fc877efc72",
+    "url": "/react_add-todo-form/static/css/main.d00c68d1.chunk.css"
+  },
+  {
+    "revision": "8c62e9e39566e04c3c1e",
+    "url": "/react_add-todo-form/static/js/2.263e448f.chunk.js"
+  },
+  {
+    "revision": "a214322a08fc877efc72",
+    "url": "/react_add-todo-form/static/js/main.eac2d0e6.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
